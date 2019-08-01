0a53a92849607d91abd20ecd962f91b3215a2bd6
Maria <maria.karamina@gmail.com>
2019-08-01 16:48:15 +0300

    HomePage draft

--- TED_frontend/src/Constants/Constants.js
+++ TED_frontend/src/Constants/Constants.js
@@ -1,4 +1,4 @@
 
 
-//export const BASEURL = "https://localhost:8443"
-export const BASEURL = "https://192.168.178.186:8443"
+export const BASEURL = "https://localhost:8443"
+//export const BASEURL = "https://192.168.178.186:8443"

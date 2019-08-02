0fd59e519146d64e32396cfd795345a6d4b3e845
Maria <maria.karamina@gmail.com>
2019-08-02 19:00:01 +0300

    Slightly longer search bar

--- TED_frontend/src/Elements/Header.js
+++ TED_frontend/src/Elements/Header.js
@@ -4,7 +4,7 @@ import React from "react"
 function Header() {
 	return (
 		<div className="title-header">
-			<img style={{verticalAlign: "middle"}} src={require("../images/white_hammer_icon_smaller.png")} alt="logo" />
+			<img style={{verticalAlign: "middle"}} src={require("../images/white_hammer_icon_30px.png")} alt="logo" />
 		{/*	<img style={{verticalAlign: "middle"}} src={require("../images/cow_logo1_68_white.png")} alt="logo" />*/}
 			<span className="header-text">Bidder</span>
 		</div>

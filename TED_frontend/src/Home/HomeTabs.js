58707277cd8f96a99d6d7218382be7e56a050c8e
Maria <maria.karamina@gmail.com>
2019-08-27 18:56:23 +0300

    with admin tab

--- TED_frontend/src/Home/HomeTabs.js
+++ TED_frontend/src/Home/HomeTabs.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react"
-import {Tabs, Tab} from 'react-bootstrap-tabs';
+import {Tabs, Tab} from 'react-bootstrap-tabs'
 import Feed from "./Feed"
+import AuthHelper from "../utils/AuthHelper"
 
 class HomeTabs extends Component {
+
 	render() {
+
 		return (
 			<Tabs className="home-tabs" headerClass="home-tabs-header" activeHeaderClass="home-tabs-active" defaultActiveKey="profile" id="uncontrolled-tab-example">
 			 	<Tab label="Feed">
@@ -12,6 +15,13 @@ class HomeTabs extends Component {
 			 	<Tab label="Auction Managment">
 			    	<div>Hey guys!</div>
 				</Tab>
+
+				{AuthHelper.isAdmin() ?
+					<Tab label="Account Managment">
+						<div>Page only accessible to the admin</div>
+					</Tab>
+					: <Tab />
+				}
 			</Tabs>
 		)
 	}

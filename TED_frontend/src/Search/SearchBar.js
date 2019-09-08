e5c5822ac05274a9b156f09b48a7eb1463b2fd2e
Maria <maria.karamina@gmail.com>
2019-09-09 01:05:41 +0300

    SearchBar without search

--- TED_frontend/src/Search/SearchBar.js
+++ TED_frontend/src/Search/SearchBar.js
@@ -1,19 +1,83 @@
 import React, { Component } from "react"
+import { customRequest } from "../utils/AuthHelper"
+import { displayError } from "../utils/ErrorHelper"
 
 class SearchBar extends Component {
 
 	constructor() {
 		super()
 		this.state = {
-			text: ""
+			text: "",
+			keyPresses: 1,
+			suggestions: [],
+			displaySuggestions: true,
 		}
 
-		this.handleSubmit = this.handleSubmit.bind(this)
+		this.getSuggestions = this.getSuggestions.bind(this)
 		this.handleChange = this.handleChange.bind(this)
+		this.handleFocus = this.handleFocus.bind(this)
+		this.handleBlur = this.handleBlur.bind(this)
+		this.handleSuggestSelect = this.handleSuggestSelect.bind(this)
+		this.handleSubmit = this.handleSubmit.bind(this)
+
+	}
+
+	getSuggestions(word) {
+		console.log("Getting suggestions for: ", word)
+		customRequest("GET", `/search/partialMatch?keyword=${word}`)
+		.then(response => {
+			console.log("response: ", response)
+			console.log("response.data: ", response.data)
+			this.setState({
+				suggestions: response.data,
+			})
+		}).catch(err => {
+			displayError(err)
+		})
 	}
 
 	handleChange(event) {
-		console.log("poop")
+		const value = event.target.value
+		const pos = value.lastIndexOf(" ")
+		const lastWord = value.slice(pos+1)
+		const keyPresses = this.state.keyPresses + 1 % 10
+
+		this.setState({
+			text: value,
+			keyPresses: keyPresses,
+		})
+
+		if(lastWord.length > 2 && keyPresses % 2 === 0) {
+			this.getSuggestions(lastWord)
+		}
+		else {
+			this.setState({
+				suggestions: [],
+			})
+		}
+	}
+
+	handleFocus() {
+		this.setState({
+			displaySuggestions: true,
+		})
+	}
+
+	handleBlur() {
+		setTimeout(() => {
+			this.setState({
+				displaySuggestions: false,
+			})
+		}, 100)
+		
+	}
+
+	handleSuggestSelect(event) {
+		const { id, textContent } = event.target
+		console.log("textContent: ", textContent)
+		this.setState({
+			text: textContent,
+		})
 	}
 
 	handleSubmit(event) {
@@ -22,17 +86,33 @@ class SearchBar extends Component {
 	}
 
 	render() {
+		let suggestionBox = null
+		const suggestions = this.state.suggestions.map((suggestion, index) => {
+			return (
+				<div key={index} className="search-suggestion" onClick={this.handleSuggestSelect}>{suggestion}</div>
+			)
+		})
+
+		if(this.state.displaySuggestions && suggestions.length > 0) {
+			suggestionBox = 
+				<div className="search-suggestion-list">
+					{suggestions}
+				</div>
+		}
+
 		return (
-			<form onSubmit={this.handleSubmit}>
+			<form onSubmit={this.handleSubmit} onBlur={this.handleBlur} onFocus={this.handleFocus}>
 				<input
 					type="text"
 					className="search-bar"
 					placeholder="Search"
+					value={this.state.text}
 					onChange={this.handleChange}
 				 />
-				 <button className="main-button" type="submit">
-				 	Search
-				 </button>
+				 <button className="main-button" type="submit">Search</button>
+				 <div className="search-suggestion-container">
+					 {suggestionBox}
+				 </div>
 			</form>
 		)
 	}

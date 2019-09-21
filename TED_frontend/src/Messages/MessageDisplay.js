b076090ec4bff55503f8ba32fcac42a3e5157e4a
Maria <maria.karamina@gmail.com>
2019-09-21 18:08:42 +0300

    Delete message

--- TED_frontend/src/Messages/MessageDisplay.js
+++ TED_frontend/src/Messages/MessageDisplay.js
@@ -1,6 +1,52 @@
 import React, { Component } from "react"
+import Swal from "sweetalert2"
+import { customRequest } from "../utils/AuthHelper"
+import { displayError } from "../utils/ErrorHelper"
 
 class MessageDisplay extends Component {
+	constructor() {
+		super()
+
+		this.submitDelete = this.submitDelete.bind(this)
+		this.verifyDelete = this.verifyDelete.bind(this)
+		this.deleteMessage = this.deleteMessage.bind(this)
+	}
+
+	submitDelete() {
+		customRequest("PATCH", `/user/deleteMessage/${this.props.message.id}`)
+		.then(response => {
+			Swal.fire({
+				title: "Success",
+				text: "Message Deleted",
+				type: 'success',
+			}).then(result => {
+				window.location.reload()
+			})
+		}).catch(err => {
+			displayError(err)
+		})
+	}
+
+	verifyDelete() {
+    	Swal.fire({
+    		title: 'Are you sure?',
+    		text: "Delete Message?",
+    		type: 'warning',
+    		showCancelButton: true,
+    		confirmButtonColor: '#3085d6',
+    	 	cancelButtonColor: '#d33',
+    		confirmButtonText: 'Delete Message'
+    	}).then(result => {
+    		if(result.value) {
+    			this.submitDelete()
+    		}
+    	})
+    }
+
+	deleteMessage() {
+		this.verifyDelete()
+	}
+
 	render() {
 		const message = this.props.message
 		return (
@@ -15,7 +61,8 @@ class MessageDisplay extends Component {
 						<label className="detail-field">From:</label> {message.sender.username}<br />
 						<label className="detail-field">To:</label> {message.recipient.username}<br />
 						<label className="detail-field">About:</label> Auction #{message.item.id} (<a href={`/auctions/${message.item.id}`}>{message.item.name}</a>)<br />
-						{!this.props.sent && <button className="reply-button" onClick={() => this.props.reply(this.props.index)}>Reply</button>}
+						{!this.props.sent && <button className="reply-button" onClick={() => this.props.reply(this.props.index)}>Reply |&nbsp;</button>}
+						<button className="reply-button" onClick={this.deleteMessage}>Delete</button>
 					</div>
 					<br />
 					<br />
--- TED_frontend/src/Messages/MessagePreview.js
+++ TED_frontend/src/Messages/MessagePreview.js
@@ -1,6 +1,52 @@
 import React, { Component } from "react"
+import Swal from "sweetalert2"
+import { customRequest } from "../utils/AuthHelper"
+import { displayError } from "../utils/ErrorHelper"
 
 class MessagePreview extends Component {
+	constructor() {
+		super()
+
+		this.submitDelete = this.submitDelete.bind(this)
+		this.verifyDelete = this.verifyDelete.bind(this)
+		this.deleteMessage = this.deleteMessage.bind(this)
+	}
+
+	submitDelete() {
+		customRequest("PATCH", `/user/deleteMessage/${this.props.message.id}`)
+		.then(response => {
+			Swal.fire({
+				title: "Success",
+				text: "Message Deleted",
+				type: 'success',
+			}).then(result => {
+				window.location.reload()
+			})
+		}).catch(err => {
+			displayError(err)
+		})
+	}
+
+	verifyDelete() {
+    	Swal.fire({
+    		title: 'Are you sure?',
+    		text: "Delete Message?",
+    		type: 'warning',
+    		showCancelButton: true,
+    		confirmButtonColor: '#3085d6',
+    	 	cancelButtonColor: '#d33',
+    		confirmButtonText: 'Delete Message'
+    	}).then(result => {
+    		if(result.value) {
+    			this.submitDelete()
+    		}
+    	})
+    }
+
+	deleteMessage() {
+		this.verifyDelete()
+	}
+
 	render() {
 		const fromTo = this.props.sent ? "To " + this.props.message.recipient.username : "From " + this.props.message.sender.username
 		return (
@@ -8,6 +54,8 @@ class MessagePreview extends Component {
 				<button className={"link-button " + this.props.type} onClick={() => this.props.onClick(this.props.index)}>
 					{fromTo} about item #{this.props.message.item.id} ({this.props.message.item.name})
 				</button>
+				<br />
+				<button className="reply-button" onClick={this.deleteMessage}>Delete</button>
 			</div>
 		)
 	}
